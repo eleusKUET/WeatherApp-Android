810e440f4e26ba063e9b0da0b74c220deb9b41aa
Eleus Ahammad <eleusahmed4@gmail.com>
2023-03-19 00:30:52 +0600

    I have added a myclass.java file

---
+++ app/src/main/java/com/aniketjain/weatherapp/MyClass.java
@@ -0,0 +1,14 @@
+package com.aniketjain.weatherapp;
+
+public class MyClass {
+    private int n;
+    MyClass(int n)  {
+        this.n = n;
+    }
+    public void setval(int a) {
+        n = a;
+    }
+    int getval() {
+        return n;
+    }
+}
